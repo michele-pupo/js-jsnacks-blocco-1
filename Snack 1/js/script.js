f740e77110467a392ab7bca724bbcc581285d4ef
michele-pupo <mikypupo@hotmail.it>
2024-02-02 14:19:27 +0100

    finished the first snack without stylization

---
+++ Snack 1/js/script.js
@@ -0,0 +1,23 @@
+// Snack 1:
+// - L’utente inserisce due parole in successione, con due prompt.
+// - Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// dasdsasdprima parola inserita dall'utente
+const word1 = prompt("Scrivi la prima parola");
+
+// seconda parola inserita dall'utente
+const word2 = prompt("Scrivi la seconda parola");
+
+
+if (word1.length > word2.length) {
+
+    document.querySelector("#outcome").innerHTML = `La prima parola é più lunga!`;
+
+} else if (word1.length < word2.length){
+
+    document.querySelector("#outcome").innerHTML = `La seconda parola é più lunga!`;
+
+} else {
+
+    document.querySelector("#outcome").innerHTML = `Le due parole hanno la stessa lunghezza!!`;
+}    
